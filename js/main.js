1d49ee4c6338a8a0664263db737fdb3f0862d15d
chassisGH <108310210+chassisGH@users.noreply.github.com>
2023-01-05 21:12:54 -0600

    enwnew

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    
-    burger.addEventListener('click', () => {
-        //toggle nav
-        nav.classList.toggle('nav-active');
-    
-        //animate links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease fowards ${index / 7 + 1}s`;
-            }
-        });
-        //burger animation
-        burger.classList.toggle('toggle');
-    });
-
-
-}
-
-navSlide();
-
 //sparkle
 let index = 0,
     interval = 1000;
